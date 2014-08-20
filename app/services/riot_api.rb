b67d8d215328a45b30a61db3f6db4caa8754d826
James Liu <jaycliu@gmail.com>
2014-08-19 19:45:56 -0700

    create two methods, for retrieving data on free champions

--- app/services/riot_api.rb
+++ app/services/riot_api.rb
@@ -1,3 +1,12 @@
-class RiotAPI
-  
+module RiotAPI
+  BASE_URI = "https://na.api.pvp.net/api/lol/na"
+  def self.riot_get endpoint
+    JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}"
+  end
+
+  def self.get_free_champions
+    VERSION = 'v1.2'
+    endpoint = "/#{VERSION}/champion?api_key=#{RIOT_KEY}"
+    riot_get endpoint
+  end
 end
