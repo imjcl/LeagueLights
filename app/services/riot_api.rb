53a6dd65736dc978c2faa80954a617086d4c8d2b
James Liu <jaycliu@gmail.com>
2014-08-19 20:05:56 -0700

    add static data endpoint method, refactor slightly for region

--- app/services/riot_api.rb
+++ app/services/riot_api.rb
@@ -1,12 +1,22 @@
 module RiotAPI
-  BASE_URI = "https://na.api.pvp.net/api/lol/na"
+  BASE_URI = "https://na.api.pvp.net/api/lol/"
   def self.riot_get endpoint
     JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}"
   end
 
-  def self.get_free_champions
+  module Champion
     VERSION = 'v1.2'
-    endpoint = "/#{VERSION}/champion?api_key=#{RIOT_KEY}"
-    riot_get endpoint
+    def self.get_free_champions
+      endpoint = "na/#{VERSION}/champion?freeToPlay=true&api_key=#{ENV['RIOT_KEY']}"
+      RiotAPI::riot_get endpoint
+    end
+  end
+
+  module StaticData
+    VERSION = 'v1.2'
+    def self.get_champion_static_data
+      endpoint = "static-data/na/v1.2/champion?champData=info&api_key=#{ENV['RIOT_KEY']}"
+      RiotAPI::riot_get endpoint
+    end
   end
 end
