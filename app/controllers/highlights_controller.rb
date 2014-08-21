79288d52ac490d8299aaf0a2f7a5b64f94cd96ac
James Liu <jaycliu@gmail.com>
2014-08-21 12:30:15 -0700

    create champions_reel method which should create a highlight reel for this champion

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -2,4 +2,9 @@ class HighlightsController < ApplicationController
   def index
     @champions = LeagueLights.match_free_champions
   end
+
+  def champion_reel
+    champion = params[:champion] || ""
+    @reel = GoogleService::get_highlights champion
+  end
 end
