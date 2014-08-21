45056a13588feb126c3b3381537f278284cce227
James Liu <jaycliu@gmail.com>
2014-08-21 11:05:05 -0700

    connect to youtube api and get sorted videos, right now hardcoded

---
+++ app/services/google_service.rb
@@ -0,0 +1,35 @@
+require "google/api_client"
+module GoogleService
+  def self.test
+    client = Google::APIClient.new(key: ENV['GOOGLE_API_KEY'], authorization: nil)
+    youtube = client.discovered_api('youtube', 'v3')
+    opts = {}
+    opts[:part] = 'id,snippet'
+    opts[:q] = 'ahri'
+    opts[:maxResults] = 25
+    opts[:publishedAfter] = six_months_ago
+    opts[:order] = 'viewCount'
+
+    videos = []
+    result = client.execute(api_method: youtube.search.list, parameters: opts)
+    result.data.items.each do |sr|
+      case sr.id.kind
+        when 'youtube#video'  
+          videos.push("#{sr.snippet.title} (#{sr.id.videoId})")
+      end
+    end
+
+    videos
+  end
+
+  def self.six_months_ago
+    now = DateTime.now
+    n_month = DateTime.now.mon
+    n_year  = DateTime.now.year
+
+    month = (n_month - 6) <= 0 ? 12 + n_month : n_month
+    year  = (n_month - 6) <= 0 ? n_year - 1 : n_year
+
+    DateTime.new(year, month).rfc3339
+  end
+end
