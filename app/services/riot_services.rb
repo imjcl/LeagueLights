473f326f8f5a77d5466c22547b9cd99b699d588f
James Liu <jaycliu@gmail.com>
2014-08-26 01:22:29 -0700

    setup method to grab spells relating to this particular champion

--- app/services/riot_services.rb
+++ app/services/riot_services.rb
@@ -1,4 +1,9 @@
 module RiotServices
-  def self.match_champion_to_id
+  def self.match_champion_to_id champion_name
+    champion_info = (RiotAPI::StaticData.get_champion_static_data)["data"]
+    match = champion_info.select { |k, v| k == champion_name.capitalize }
+    id = match[champion_name.capitalize]["id"]
+
+    RiotAPI::StaticData.get_champion_abilities id
   end
 end
