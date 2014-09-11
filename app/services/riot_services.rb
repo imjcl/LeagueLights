35ec8ec2e9fd57e68b159532c0d0e6e377a1334f
James Liu <jaycliu@gmail.com>
2014-09-11 15:42:05 -0700

    create match_champion_to_id to make sure that we can retrieve ids to match with the names

--- app/services/riot_services.rb
+++ app/services/riot_services.rb
@@ -1,8 +1,11 @@
 module RiotServices
   def self.match_champion_to_id champion_name
     champion_info = (RiotAPI::StaticData.get_champion_static_data)["data"]
-    match = champion_info.select { |k, v| k == champion_name.capitalize }
-    id = match[champion_name.capitalize]["id"]
+    match = champion_info.select do |k, v| 
+      k.downcase == champion_name
+    end
+
+    id = match[match.keys[0]]["id"]
 
     RiotAPI::StaticData.get_champion_abilities id
   end
