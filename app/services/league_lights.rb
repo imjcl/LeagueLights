3156921cccb5b3863de89a28cbb2b90e3d1f7182
James Liu <jaycliu@gmail.com>
2014-09-17 10:17:41 -0700

    move match_champion_to_id to leaguelights, from riot_services

--- app/services/league_lights.rb
+++ app/services/league_lights.rb
@@ -12,4 +12,15 @@ module LeagueLights
     
     champion_data
   end
+  
+  def self.match_champion_to_id champion_name
+    champion_info = (RiotAPI::StaticData.get_champion_static_data)["data"]
+    match = champion_info.select do |k, v| 
+      k.downcase == champion_name
+    end
+
+    id = match[match.keys[0]]["id"]
+
+    RiotAPI::StaticData.get_champion_abilities id
+  end  
 end
