8b461327cd121d30f7e5f2080b9b93b1e711bc6b
James Liu <jaycliu@gmail.com>
2014-09-18 10:04:31 -0700

    call worker if you can't find the key value in redis

--- app/services/league_lights.rb
+++ app/services/league_lights.rb
@@ -1,13 +1,7 @@
 module LeagueLights
   def self.match_free_champions
     unless REDIS.exists 'free_ids'
-      REDIS.set 'free_ids', RiotAPI::Champion.get_free_champions
-      REDIS.expire 'free_ids', 432000
-    end
-
-    unless REDIS.exists 'static_data'
-      REDIS.set 'static_data', RiotAPI::StaticData.get_champion_static_data
-      REDIS.expire 'static_data', 432000
+      RiotWorker.perform_async
     end
 
     free_ids = JSON.parse REDIS.get 'free_ids'
