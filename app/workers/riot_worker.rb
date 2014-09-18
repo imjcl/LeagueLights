b4d78e93a2a2e500ac95ec464d2d8a4bbe274767
James Liu <jaycliu@gmail.com>
2014-09-18 10:03:54 -0700

    move some code logic from leaguelights over to make sure that cache is up to date

--- app/workers/riot_worker.rb
+++ app/workers/riot_worker.rb
@@ -1,7 +1,18 @@
 class RiotWorker
   include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-  def perform(name, count)
-    puts 'Doing Riot work!'
+  recurrence { daily }
+
+  def perform
+    unless REDIS.exists 'free_ids'
+      REDIS.set 'free_ids', RiotAPI::Champion.get_free_champions
+      REDIS.expire 'free_ids', 432000
+    end
+
+    unless REDIS.exists 'static_data'
+      REDIS.set 'static_data', RiotAPI::StaticData.get_champion_static_data
+      REDIS.expire 'static_data', 432000
+    end
   end
 end
